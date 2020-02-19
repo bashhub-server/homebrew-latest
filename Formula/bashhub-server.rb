8e48b30ce312658f2738a113c0a4e8c53ea3d8a1
nicksherron <nsherron90@gmail.com>
2020-02-19 15:09:56 -0500

    version 0.2.4

--- Formula/bashhub-server.rb
+++ Formula/bashhub-server.rb
@@ -1,16 +1,16 @@
 class BashhubServer < Formula
   desc "privately hosted open source server for bashhub-client https://github.com/rcaloras/bashhub-client"
   homepage ""
-  version "0.2.3"
+  version "0.2.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/nicksherron/bashhub-server/releases/download/v0.2.3/bashhub-server_v0.2.3_darwin_amd64.tar.gz"
-    sha256 "da8309afc16e35f23f92a4a4fdd66508824e3427f9433cac3548436f6ff29018"
+    url "https://github.com/nicksherron/bashhub-server/releases/download/v0.2.4/bashhub-server_v0.2.4_darwin_amd64.tar.gz"
+    sha256 "2c5d3b5790dadf3cb9d6b750fb66b6d11572b7a457ed1faf6b02c19c61149670"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/nicksherron/bashhub-server/releases/download/v0.2.3/bashhub-server_v0.2.3_linux_amd64.tar.gz"
-      sha256 "3d3e816811ea6bf31de9fd31b52ebd586ebbad53c44bc61aa81143c79cd692a5"
+      url "https://github.com/nicksherron/bashhub-server/releases/download/v0.2.4/bashhub-server_v0.2.4_linux_amd64.tar.gz"
+      sha256 "2e0ff6aa91cc82ba6e0f37db4efad42b1e9cf0b11f51380c246a99f8a32390b4"
     end
   end
 
